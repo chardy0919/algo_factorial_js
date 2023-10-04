92f4ead12500d59ad3c36c99605926b3cd75d328
Cody Hardy <chardy0919@gmail.com>
2023-10-04 16:15:01 -0400

    fleshed out a factorial function

--- factorial.js
+++ factorial.js
@@ -1,5 +1,13 @@
 function factorial(num) {
-  return 0;
+  if (num == 0){
+    return 1
+  }else{
+    let answer = 1
+    while (num > 0){
+      answer *= num
+    };
+    return answer
+  };
 }
 
 module.exports = factorial;
